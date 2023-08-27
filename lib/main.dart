76b243a9aa9a1887fb65ff6b6c09b7a1afdd99ab
Avishka Prabath <avishka2k@gmail.com>
2023-08-27 13:53:43 +0530

    login

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
        // useMaterial3: true,
       ),
-      home: SignupView(),
+      home: const AuthChanges(),
     );
   }
 }
