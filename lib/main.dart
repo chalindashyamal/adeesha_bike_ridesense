d276970c5f0e451c63d297ae125808bac94aec95
saimaboy <101470406+saimaboy@users.noreply.github.com>
2023-08-29 04:21:03 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
-import 'package:adeesha_bike_ridesense/View/history.dart';
-import 'package:adeesha_bike_ridesense/View/triplist.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'View/Evidence/Evidence.dart';
-import 'View/loyality.view.dart';
-import 'View/map.dart';
-import 'View/menu.dart';
-import 'View/setting.dart';
 import 'components/auth_state_changes.dart';
 import 'firebase_options.dart';
 
@@ -32,7 +25,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
        // useMaterial3: true,
       ),
-      home:  AuthChanges(),
+      home:  const AuthChanges(),
     );
   }
 }
