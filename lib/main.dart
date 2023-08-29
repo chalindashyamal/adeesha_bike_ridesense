bb10360af256ca7b3ec08715ed8f3b485bbda256
saimaboy <101470406+saimaboy@users.noreply.github.com>
2023-08-29 06:22:41 +0530

    hh

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
        // useMaterial3: true,
       ),
-      home: MapWithLocationName(),
+      home: AuthChanges(),
     );
   }
 }
